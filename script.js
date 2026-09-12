6bc6ea1e415280d84b554eb04fb695c091202124
drjcs2020 <76589598+drjcs2020@users.noreply.github.com>
2026-09-12 14:18:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function iniciarMissao() {
+
+    alert(
+    "🏰 Bem-vindo ao Reino ClassMana!\n\n" +
+    "Os antigos mestres esconderam uma mensagem dentro do Baú dos Enigmas.\n\n" +
+    "Sua guilda deverá resolver desafios de Matemática, Informática e Lógica para quebrar os selos mágicos."
+    );
+
+}
